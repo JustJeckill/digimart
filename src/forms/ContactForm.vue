c666615b464cac79f65437580b1eaff1b670eb03
JustJeckill <yevhenduplenko@gmail.com>
2025-02-16 23:38:06 -0700

    [digimart] add Contact Form to project, install docker, confi is in progress

---
+++ src/forms/ContactForm.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <form action="/" method="post" class="contact-form">
+    <div class="contact-form__fieldset">
+      <input type="text" name="contact-name" class="contact-form__field" placeholder="Your name">
+      <span class="contact-form__error"></span>
+    </div>
+    <div class="contact-form__fieldset">
+      <input type="text" name="contact-email" class="contact-form__field" placeholder="Your email">
+      <span class="contact-form__error"></span>
+    </div>
+    <div class="contact-form__fieldset">
+      <input type="text" name="contact-message" class="contact-form__field" placeholder="Your message">
+      <span class="contact-form__error"></span>
+    </div>
+    <div class="contact-form__action">
+      <button type="button" class="contact-form__button">Submit</button>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="contact">
+    <h1 class="contact__title">Contact Us</h1>
+    <p class="contact__subtitle">Send the form to us and our manager will answer to yuo soon!</p>
+
+    <ContactForm/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
